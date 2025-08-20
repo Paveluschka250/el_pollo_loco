7899bcd43a87aea33a1957318224443021b37025
Paul Gross <p.gross6408@gmail.com>
2025-08-20 16:35:02 +0200

    feat: add addToMap function and simplify draw method

---
+++ models/background.class.js
@@ -0,0 +1,11 @@
+class Background extends MovableObject {
+
+    constructor() {
+        super();
+        // this.loadImage('assets/img/5_background/layers/1.png');
+        this.x = 0;
+        this.y = 0;
+        this.width = 720;
+        this.height = 480;
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -1,27 +1,30 @@
 class World {
-    canvas
-    ctx;
-    character = new Character();
-    chicken1 = new Chicken();
-    chicken2 = new Chicken();
-    chicken3 = new Chicken();
-    clouds = [new Cloud(), new Cloud(), new Cloud()];
+  canvas;
+  ctx;
+  character = new Character();
+  chickens = [new Chicken(), new Chicken(), new Chicken()];
+  clouds = [new Cloud(), new Cloud(), new Cloud()];
+  background = new Background();
 
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.ctx = canvas.getContext("2d");
-        this.draw();
-    }
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.draw();
+  }
 
-    draw() {
-        this.ctx.clearRect(0, 0, canvas.width, canvas.height);
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
-        this.ctx.drawImage(this.chicken1.img, this.chicken1.x, this.chicken1.y, this.chicken1.width, this.chicken1.height);
-        this.ctx.drawImage(this.chicken2.img, this.chicken2.x, this.chicken2.y, this.chicken2.width, this.chicken2.height);
-        this.ctx.drawImage(this.chicken3.img, this.chicken3.x, this.chicken3.y, this.chicken3.width, this.chicken3.height);
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        requestAnimationFrame(this.draw.bind(this));
-    }
+  draw() {
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.clouds.forEach((cloud) => {
+      this.addToMap(cloud);
+    });
+    this.chickens.forEach((chicken) => {
+      this.addToMap(chicken);
+    });
+    this.addToMap(this.character);
+    requestAnimationFrame(this.draw.bind(this));
+  }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+  }
 }
