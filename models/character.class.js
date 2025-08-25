0b521619d1f86f8e748b93236747c19e7c05b47c
Paul Gross <p.gross6408@gmail.com>
2025-08-25 16:40:55 +0200

    feat: add multiple background layers

--- models/character.class.js
+++ models/character.class.js
@@ -4,7 +4,7 @@ class Character extends MovableObject {
         super();
         this.loadImage('assets/img/2_character_pepe/1_idle/idle/I-1.png');
         this.x = 100; // Initial x position
-        this.y = 200; // Initial y position
+        this.y = 225; // Initial y position
         this.width = 120; // Initial width
         this.height = 200; // Initial height
     }
--- models/world.class.js
+++ models/world.class.js
@@ -5,7 +5,11 @@ class World {
   chickens = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud(), new Cloud(), new Cloud()];
   background = [
+    new Background("assets/img/5_background/layers/air.png"),
+    new Background("assets/img/5_background/layers/3_third_layer/1.png"),
+    new Background("assets/img/5_background/layers/2_second_layer/1.png"),
     new Background("assets/img/5_background/layers/1_first_layer/1.png"),
+
   ];
 
   constructor(canvas) {
