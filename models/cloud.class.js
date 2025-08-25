6dda1ab3966a1907a79e9f891b1ff5e4b9ceae8b
Paul Gross <p.gross6408@gmail.com>
2025-08-25 16:55:00 +0200

    feat: add animation to Cloud class

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,5 +7,13 @@ class Cloud extends MovableObject {
         this.y = Math.random() * 50; 
         this.width = 300;
         this.height = 150;
+        this.animate();
+    }
+
+    animate() {
+      setInterval(() => {
+        this.x -= 0.5;
+        if (this.x < -300) this.x = 700;
+      }, 1000 / 60);
     }
 }
