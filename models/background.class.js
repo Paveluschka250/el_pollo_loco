cf9c87718cfca37f4491ddb15e8fa7eb63c749a3
Paul Gross <p.gross6408@gmail.com>
2025-08-26 18:02:39 +0200

    feat: add multiple background layers

--- models/background.class.js
+++ models/background.class.js
@@ -1,9 +1,9 @@
 class Background extends MovableObject {
 
-    constructor(imagePath) {
+    constructor(imagePath, x) {
         super();
-        this.loadImage(imagePath);
-        this.x = 0;
+        this.loadImage(imagePath, x);
+        this.x = x;
         this.y = 0;
         this.width = 720;
         this.height = 480;
--- models/world.class.js
+++ models/world.class.js
@@ -7,10 +7,26 @@ class World {
   chickens = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud(), new Cloud(), new Cloud()];
   background = [
-    new Background("assets/img/5_background/layers/air.png"),
-    new Background("assets/img/5_background/layers/3_third_layer/1.png"),
-    new Background("assets/img/5_background/layers/2_second_layer/1.png"),
-    new Background("assets/img/5_background/layers/1_first_layer/1.png"),
+    new Background("assets/img/5_background/layers/air.png", -720),
+    new Background("assets/img/5_background/layers/3_third_layer/2.png", -720),
+    new Background("assets/img/5_background/layers/2_second_layer/2.png", -720),
+    new Background("assets/img/5_background/layers/1_first_layer/2.png", -720),
+    new Background("assets/img/5_background/layers/air.png", 0),
+    new Background("assets/img/5_background/layers/3_third_layer/1.png", 0),
+    new Background("assets/img/5_background/layers/2_second_layer/1.png", 0),
+    new Background("assets/img/5_background/layers/1_first_layer/1.png", 0),
+    new Background("assets/img/5_background/layers/air.png", 720),
+    new Background("assets/img/5_background/layers/3_third_layer/2.png", 720),
+    new Background("assets/img/5_background/layers/2_second_layer/2.png", 720),
+    new Background("assets/img/5_background/layers/1_first_layer/2.png", 720),
+    new Background("assets/img/5_background/layers/air.png", 720 * 2),
+    new Background("assets/img/5_background/layers/3_third_layer/1.png", 720 * 2),
+    new Background("assets/img/5_background/layers/2_second_layer/1.png", 720 * 2),
+    new Background("assets/img/5_background/layers/1_first_layer/1.png", 720 * 2),
+    new Background("assets/img/5_background/layers/air.png", 720 * 3),
+    new Background("assets/img/5_background/layers/3_third_layer/2.png", 720 * 3),
+    new Background("assets/img/5_background/layers/2_second_layer/2.png", 720 * 3),
+    new Background("assets/img/5_background/layers/1_first_layer/2.png", 720 * 3),
 
   ];
 
